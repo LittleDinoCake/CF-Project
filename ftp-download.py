c3afa59a406355253af12296746bff63cef68af0
LittleDinoCake <110544952+LittleDinoCake@users.noreply.github.com>
2022-08-03 15:51:27 +0100

    Update ftp-download.py

--- ftp-download.py
+++ ftp-download.py
@@ -1,25 +1,26 @@
 import re
 import os
 from ftplib import FTP
+import argparse
 
 def downloadFiles():
     print("Connecting to FTP")
     with FTP("localhost") as ftp:
-    
+
         ftp.login()
-    
+
         # Grabs all files in specified dir
         file_list = ftp.nlst("/")
-    
+
         # Compiles a regex that matches the file name format.
         # YYYYMMDDHHMMSS is exactly 14 digits of 1-9 with MED_DATA_ before and .csv after
         # It's case-insensitive so MeD_DaTA_ and .Csv would be allowed
-    
+
         # TODO: More specific to time and date digit limits
         pattern = re.compile("(?i)(MED_DATA_)([0-9]{14})\.(csv)")
-    
+
         totalDown = 0
-    
+
         for file_name in file_list:
             if (pattern.match(file_name)):
                 print(f"Found valid medical data: {file_name}")
@@ -27,11 +28,14 @@ def downloadFiles():
                 with open(os.path.join("ftp-download-test", formatted_file_name), "wb") as f:
                     ftp.retrbinary(f"RETR {file_name}", f.write)
                     totalDown += 1
-    
+
         print(f"Downloaded {totalDown} MED_DATA files")
-        
+
 def main():
     downloadFiles()
-        
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Program Parser")
+    parser.add_argument('--date', type=datetime, help="Download all med-data from today")
+    parser.parse_args()
     main()
