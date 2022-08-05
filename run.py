75552fee260b71ad4052cfbecd23ba602869b80d
myself <myself@myself.com>
2022-08-05 14:33:51 +0100

    Added import datetime

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import argparse
 import ftpmodule as ftpm
 import fileValidation as fv
+import datetime
 
 
 
 def main(args):
 
-    ftpm.downloadFiles(ftpm.getFTPConnetion(args.target), ".temp", "/", args.date)
+    ftpm.downloadFiles(ftpm.getFTPConnetion(args.target), 
+                       ".temp", "/", args.date)
 
     # Now check files
     valid = fv.runChecks("./.temp")
@@ -27,6 +29,7 @@ def valid_date_format(s):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Program Parser")
     parser.add_argument('target', help="The target FTP server address")
-    parser.add_argument('-d', '--date', type=valid_date_format, help="Download all med-data for the given date")
+    parser.add_argument('-d', '--date', type=valid_date_format, 
+                        help="Download all med-data for the given date")
     args = parser.parse_args()
     main(args)
