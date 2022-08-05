7d97fd0a4505b6150988b549c876694f75c714dc
LittleDinoCake <110544952+LittleDinoCake@users.noreply.github.com>
2022-08-05 14:46:07 +0100

    :)
    Added some more comments
    Removed a redundant import

--- ftpmodule.py
+++ ftpmodule.py
@@ -1,7 +1,6 @@
 import re
 import os
 from ftplib import FTP
-import argparse
 from datetime import datetime
 
 def downloadFiles(ftp, local_temp_dir, remote_dir, limit_date=None):
@@ -18,7 +17,6 @@ def downloadFiles(ftp, local_temp_dir, remote_dir, limit_date=None):
     # YYYYMMDDHHMMSS is exactly 14 digits of 1-9 with MED_DATA_ before and .csv after
     # It's case-insensitive so MeD_DaTA_ and .Csv would be allowed
 
-    # TODO: More specific to time and date digit limits
     pattern = re.compile("(?i)(MED_DATA_)([0-9]{14})\.(csv)")
 
     totalDown = 0
@@ -72,6 +70,7 @@ def getFileNameDate(file):
 
 def getFTPConnetion(target):
     ftp = None
+    # Try connect to FTP and login, otherwise throw a generic error
     try:
         print("Connecting to FTP")
         ftp = FTP(target)
